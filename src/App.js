7af8b6c60ee2678bae26d99eb869300b6a7848d3
success ibekwe <Successibekwe047@gmail.com>
2023-03-22 17:19:28 +0100

    Created create listing page UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import ForgotPassword from "./pages/ForgotPassword";
+
 import Home from "./pages/Home";
-import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+
+import PrivateRoute from "./components/PrivateRoute";
+
 // ============React Toastify=============
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -17,11 +24,25 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
         </Routes>
       </Router>
 
@@ -37,6 +58,8 @@ function App() {
         pauseOnHover
         theme="dark"
       />
+      {/* Same as */}
+      {/* <ToastContainer /> */}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,27 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const Header = () => {
+export default function Header() {
+  const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
-
-  const [pageState, setPageState] = useState("sign-in");
   const auth = getAuth();
-
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setPageState("Profile");
       } else {
-        setPageState("Sign In");
+        setPageState("Sign in");
       }
     });
-  });
-
+  }, [auth]);
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 left-0 w-full z-40">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
@@ -45,7 +41,6 @@ const Header = () => {
             >
               Home
             </li>
-
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute("/offers") && "text-black border-b-red-500"
@@ -56,17 +51,8 @@ const Header = () => {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/notion") && "text-black border-b-red-500"
-              }`}
-              onClick={() => navigate("/notion")}
-            >
-              Notion
-            </li>
-
-            <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") ||
-                (pathMatchRoute("/profile") && "text-black border-b-red-500")
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
               }`}
               onClick={() => navigate("/profile")}
             >
@@ -77,6 +63,4 @@ const Header = () => {
       </header>
     </div>
   );
-};
-
-export default Header;
+}
